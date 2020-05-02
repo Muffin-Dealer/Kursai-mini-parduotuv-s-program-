8e77b474ec2ea1baf4b813df4c9985d8afacda09
mantas95 <breivemantas@gmail.com>
2020-05-02 16:31:49 +0300

    h

---
+++ MiniShop/MiniShop/Mailing.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop
+{
+    class Mailing
+    {
+        public void Internacional()
+        {
+
+        }
+        public void Courier()
+        {
+
+        }
+        public void Mail()
+        {
+
+        }
+        public void Terminal()
+        {
+
+        }
+        public double MailingPrice(ShopBasket basket)
+        {
+
+            return 0;
+        }
+        public string Size(ShopBasket basket)
+        {
+            double totalWeight = 0;
+            foreach (var item in basket.listas)
+            {
+                totalWeight += item.Weight;
+            }
+            if (totalWeight < 15)
+                return "S";
+            else if (totalWeight > 50)
+                return "L";
+            else 
+                return "M";
+
+        }
+    }
+}
---
+++ MiniShop/MiniShop/Merchandise.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop
+{
+    class Merchandise
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public double Price { get; }
+        public double Weight { get; }
+
+
+
+    }
+}
